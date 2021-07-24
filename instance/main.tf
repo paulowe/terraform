3683f3cae644b5ed8542ed3a4775d62e296a8312
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2021-07-24 18:11:24 -0400

    terrafrom fmt
    rewrtie terraform config files to a canonical format and style

--- instance/main.tf
+++ instance/main.tf
@@ -1,27 +1,27 @@
 variable "instance_name" {}
 variable "instance_zone" {}
 variable "instance_type" {
-    default = "n1-standard-1"
+  default = "n1-standard-1"
 }
 variable "instance_network" {}
 
-resource "google_compute_instance" "gnl-vm"{
-    name = "${var.instance_name}"
-    zone = "${var.instance_zone}"
-    machine_type = "${var.instance_type}"
-    # Configure the boot disk to use the Debian 9 OS image.
-    boot_disk {
-      initialize_params{
-          image = "debian-cloud/debian-9"
-      }
+resource "google_compute_instance" "gnl-vm" {
+  name         = var.instance_name
+  zone         = var.instance_zone
+  machine_type = var.instance_type
+  # Configure the boot disk to use the Debian 9 OS image.
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
     }
-    # Define the VPC
-    network_interface {
-      network = "${var.instance_network}"
-      access_config{
-          # Allocate a one-to-one NAT IP to the instance  (Assign an ephemeral external IP)
-      }
+  }
+  # Define the VPC
+  network_interface {
+    network = var.instance_network
+    access_config {
+      # Allocate a one-to-one NAT IP to the instance  (Assign an ephemeral external IP)
     }
-    
+  }
+
 
 }
